0f314a98dd24ede179db940a56832176bfcd2df1
Farhod Fatayev <fataev_f@mail.ru>
2022-11-21 21:22:08 +0300

    Work with routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,53 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Для избежания конфликтов следует писать более частные случаи маршрутов вначале, а потом - более общие.
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function(){
+    return 'it\'s test route';
+});
+
+    //parametrs in the function
+Route::get('/test/{id}', function($id){
+    return $id;
+});
+
+
+//a few parametrs in the function
+Route::get('/test/{id}/{name?}', function($id, $name = 'asd'){
+    return $id . $name;
+}) -> where('id', '[0-9]+');        //накладываем ограничение на параметр
+
+//шаблонные ограничения
+Route::get('set/{id}', function($id){
+    return $id;
+})-> whereNumber('id');
+
+Route::get('set/{letter}', function($letter){
+    return $letter;
+})->whereAlpha('letter');
+
+Route::get('set/{letterNumber}', function($letterNumber){
+    return $letterNumber;
+})->whereAlphaNumeric('letterNumber');
+
+
+//группируем маршруты, адреса которых начинаются на одинаковую часть. 
+Route::prefix('admin') -> group(function(){
+    Route::get('users', function(){
+        return 'all';
+    });
+
+    Route::get('user/{id}', function($id){
+        return $id;
+    });
+});
+
+//маршрутам можно давать имена
+Route::get('/post/all', function () {
+    return 'all';
+})->name('posts');
